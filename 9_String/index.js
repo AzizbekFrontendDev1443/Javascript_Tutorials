24d7ccbd8e97ebbba1fd42cf15d864f0784917e1
AzizbekFrontendDev1443 <azizbekfrontenddev404@gmail.com>
2023-05-22 15:33:33 +0500

    Add String

---
+++ 9_String/index.js
@@ -0,0 +1,58 @@
+// let text = "Lorem ipsum dolor, sit amet consectetur";
+// let text1 = "We are the so-called \"Vikings\"  ";
+// let text3 =
+//   "We are the-colled \
+//   hqllo \
+// ";
+
+let text = "BananabA";
+let text1 = "Kivi";
+let text2 = "Apple";
+let text3 = " Trim ";
+/* ------------------------------------------- length ------------------------------------------- */
+// let result = text.length;
+
+/* ------------------------------------------- slice() ------------------------------------------- */
+/* let result = text.slice(2, 4); // Slice out a portion of a string from position 2 toposition 4
+let result1 = text.slice("2"); // If you omit the second parametr, the method will slice out the rest of the string
+let result2 = text.slice(-4); // if a parametr is negative, the position is counted from the end of the string
+let result3 = text.slice(-3, -1); // this example slice out a portion of a string from position -3 to postion -1 */
+
+/* ------------------------------------------- replace() ------------------------------------------- */
+// let result = text.replace("b", "a"); // method replaces a specified value in a string
+// let result1 = text.replace(/b/i, "a");
+
+/* ------------------------------------------- replaceAll() ------------------------------------------- */
+/* let result = text.replaceAll("a", "b"); */
+
+/* ------------------------------------------- concat() ------------------------------------------- */
+/* let result = text.concat(text1);
+let result1 = text.concat(" " + text1 + " " + text2); */
+
+/* ------------------------------------------- trim() ------------------------------------------- */
+/* let result = text3.trim(); // method removes whiteSpace from both sides of string */
+
+/* ------------------------------------------- padStart() ------------------------------------------- */
+/* let result = text.padStart(10, "0"); // pad string with "0" until it reaches the length: 10 */
+
+/* ------------------------------------------- padEnd() ------------------------------------------- */
+/* let result = text.padEnd(10, "0"); // pad string with "0" until it reaches the length: 10 */
+
+/* ------------------------------------------- charAt() ------------------------------------------- */
+/* let result = text.charAt(0); // method returns the character at aspecified index (position) in a string */
+
+/* ------------------------------------------- split() ------------------------------------------- */
+// let result = text.split(""); // a string can be converted to an array with the split()
+
+/* ------------------------------------------- indexOf() ------------------------------------------- */
+// let result = text.indexOf("a"); // method returns the index(position) the first occurrence of a string in a string
+
+/* ------------------------------------------- lastIndexOf() ------------------------------------------- */
+// let result = text.lastIndexOf("a"); // method returns the index of the last occurrence of a specified text in a string
+
+/* ------------------------------------------- search() ------------------------------------------- */
+// let result = text.search("b");
+/* console.log(result);
+console.log(result1);
+console.log(result2);
+console.log(result3); */
