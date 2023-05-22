a4a774c488a1703c7974aeb833f4671b83644066
AzizbekFrontendDev1443 <azizbekfrontenddev404@gmail.com>
2023-05-22 13:19:07 +0500

    Add Variable scopes

---
+++ 8_Variable_Scopes/index.js
@@ -0,0 +1,19 @@
+/* ------------------------------- Block Scopes ------------------------------- */
+/*
+{
+  let userName = "Azizbek";
+}
+// userName can Not be used here
+ */
+
+/* ------------------------------- Function Scopes ------------------------------- */
+/*
+function myFunction() {
+  let age = 18;
+  // code here can use age
+}
+// code here can Not use age
+ */
+
+/* ------------------------------- Global Scopes ------------------------------- */
+let age = 17; // global
