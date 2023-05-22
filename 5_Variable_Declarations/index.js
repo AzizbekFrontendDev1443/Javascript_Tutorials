da10865719e93985f82187e9d762ab0633a698ca
AzizbekFrontendDev1443 <azizbekfrontenddev404@gmail.com>
2023-05-22 10:40:28 +0500

    Add Variable Declarations

---
+++ 5_Variable_Declarations/index.js
@@ -0,0 +1,36 @@
+/* -------------------------------------- Let -------------------------------------- */
+/*
+let x = "Azizbek"; // Allowed
+// let x=12 // Not Allowed
+ */
+/* {
+  let y = 12;
+  console.log(y); // here y is 12
+}
+console.log(y); // y can Not be used here
+*/
+/*
+let x = 12;
+console.log(x); // here x is 12
+{
+  let x = 10; // Allowed
+  console.log(x); // here x is 10
+}
+console.log(x); // here x is 12
+ */
+
+/* -------------------------------------- Const -------------------------------------- */
+/*
+const PI = 3.141592;
+// PI = 3.14; // This will give an arror
+// PI = PI + 10; // This will also give an error
+ */
+/*
+const x = 12;
+console.log(x); // here x is 12
+{
+  const x = 10;
+  console.log(x); // here x is 10
+}
+console.log(x); // here x is 12
+ */
